4504ef195c10cd92d0de9bb137c17711cde736ba
1226choiy <1226choiy@gmail.com>
2025-11-19 14:55:48 +0900

    in process

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- 헤더 -->
     <div class="relative">
       <!-- 헤더 이미지 -->
-      <img class="w-full opacity-80 max-h-[300px]" src="/headerbg.jpg" alt="headerBG" />
+      <img class="w-full opacity-80 max-h-[250px]" src="/headerbg.jpg" alt="headerBG" />
       <!-- 로고 이미지 동그랗게 -->
       <img
         class="w-[25%] max-w-[140px] rounded-full absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2"
@@ -19,21 +19,28 @@
     <!-- 링크 버튼들 -->
     <div class="flex flex-col gap-4 items-center mt-10 md:mt-17">
       <!-- 인스타그램 -->
-      <a class="border-2 py-2 px-6 rounded-full border-[#194d29] text-[#194d29] font-bold" href="">
+      <a
+        target="_blank"
+        class="border-2 w-[200px] block py-2 text-center rounded-full border-[#194d29] text-[#194d29] font-bold hover:bg-[#194d29] hover:text-[#ffeddd]"
+        href="https://www.instagram.com/leaf.art_studio/#">
         인스타그램
       </a>
       <!-- 네이버 플레이스 -->
+      <a
+        target="_blank"
+        class="border-2 w-[200px] block py-2 text-center rounded-full border-[#194d29] text-[#194d29] font-bold hover:bg-[#194d29] hover:text-[#ffeddd]"
+        href="https://naver.me/GmtRyCsS">
+        네이버 플레이스
+      </a>
       <!-- 카카오톡 -->
+      <a
+        target="_blank"
+        class="border-2 w-[200px] block py-2 text-center rounded-full border-[#194d29] text-[#194d29] font-bold hover:bg-[#194d29] hover:text-[#ffeddd]"
+        href="http://pf.kakao.com/_ExiSxcn">
+        카카오톡
+      </a>
     </div>
   </div>
 </template>
 
 <script setup></script>
-
-<!-- .wrap {
-   margin: auto;
-   width: 100%;
-   height: 100vh;
-   background-color: #fbe3ce;
-   color: #194d29;
- } -->
